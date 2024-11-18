5f6337496065aa29dbfd17a75a0b42dcc1a8d601
hamza durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-11-18 18:33:38 +0500

    resolving errors pt1

--- src/Component/ChangepasswordProtected.jsx
+++ src/Component/ChangepasswordProtected.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const ChangepasswordProtected = ({ children }) => {
   // Check if OTP has been verified
-  const isOtpVerified = localStorage.getItem('otpVerified') === 'true';
+  const isOtpVerified = localStorage.getItem("otpVerified") === "true";
 
   if (!isOtpVerified) {
     // Redirect to OTP page if OTP not verified
@@ -14,3 +14,4 @@ const ChangepasswordProtected = ({ children }) => {
 };
 
 export default ChangepasswordProtected;
+//  trying to solve errors
