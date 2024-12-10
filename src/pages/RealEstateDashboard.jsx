7cfd1c1eba3467970d34e46b9207252060c2658c
booali9 <booali654@gmail.com>
2024-12-09 22:25:59 -0500

    bug of dashboard

--- src/pages/RealEstateDashboard.jsx
+++ src/pages/RealEstateDashboard.jsx
@@ -3,7 +3,7 @@ import { Topbar } from "../Component/dashboard/Topbar";
 import { DashboardSidebar as Sidebar } from "../Component/RealEstateDashboard/Sidebar";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import MyProfile from "../Component/PropertyOwnerDashboard/Profile/MyProfile";
+import MyProfile from "../Component/RealEstateDashboard/MyProfile/MyProfile";
 import PostForm from "../Component/serviceProvider dashboard/PostService/Postservice";
 import MyProperties from "../Component/RealEstateDashboard/MyProperties/MyProperties";
 import { Applications } from "../Component/serviceProvider dashboard/Applications/Applications";
@@ -13,6 +13,8 @@ import { HelpAndSupport } from "../Component/dashboard/Help&Support/HelpSupport"
 import { IndustryRelations } from "../Component/serviceProvider dashboard/IndustryRelations/IndustryRelations";
 import { Accounting } from "../Component/serviceProvider dashboard/Accounting/Accounting";
 import PropertyForm from "../Component/dashboard/Propertyform/Propertyform";
+import MyPropertiesServices from "../Component/RealEstateDashboard/MyProperties/MyProperties";
+import ProfileForm from "../Component/RealEstateDashboard/MyProfile/MyProfile";
 
 export function RealEstateDashboard() {
   const { section } = useParams();
@@ -37,7 +39,7 @@ export function RealEstateDashboard() {
         <div className={`flex-1 flex flex-col`}>
           <Topbar section={section} />
           <main className="flex-1 overflow-auto p-4">
-            {section === "My profile" && <MyProfile />}
+            {section === "My Profile" && <ProfileForm/>}
             {section === "services" && <PostForm />}
             {section === "properties" && <MyProperties />}
             {section === "applications" && <Applications />}
