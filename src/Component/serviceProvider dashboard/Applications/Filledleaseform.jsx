a2a102d471d49a188d29eb67e7032366eb2f4b7d
Hamza Durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-12-17 23:59:51 +0500

    resolving errors 4

--- src/Component/serviceProvider dashboard/Applications/Filledleaseform.jsx
+++ src/Component/serviceProvider dashboard/Applications/Filledleaseform.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { Card } from "../../../shadcn/components/ui/Card";
 
 const leftDetails = [
--- src/Component/serviceProvider dashboard/Transport/Transport.jsx
+++ src/Component/serviceProvider dashboard/Transport/Transport.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const dashboardPaths = {
   superadmin: "/superadmin/properties",
